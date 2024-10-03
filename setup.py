7ed6cb47f99e031538e0a81b3ea83ded04c24549
jcole313 <162965770+jcole313@users.noreply.github.com>
2024-10-03 22:04:12 +0000

    structure done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="multilingual assistant",
+    version="0.0.1",
+    author="Jatin",
+    author_email="sareenjatin002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
